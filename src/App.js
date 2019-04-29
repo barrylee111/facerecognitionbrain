63864216553b3274d9012ce1b249844208a56f56
barrylee111 <barrylee115@gmail.com>
2019-04-28 18:59:02 -0600

    Added particle bkgd and CSS for particles

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import Particles from 'react-particles-js';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Navigation />
-      <Logo />
-      <ImageLinkForm />
+const particlesOptions={
+  particles: {
+     number: {
+      value: 30,
+      density: {
+        enable: true,
+        value_area: 250
+      }
+     }
+  }
+}
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+        <Particles className='particles'
+          params={particlesOptions}
+        />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm />
         {/*<FaceRecognition />*/}
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default App;
