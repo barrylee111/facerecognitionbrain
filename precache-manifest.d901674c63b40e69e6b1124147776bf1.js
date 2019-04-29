c74bbdb31a9e7c88e34bbb6182048e5c8aa670ae
barrylee111 <barrylee115@gmail.com>
2019-04-28 18:59:49 -0600

    Updates

---
+++ precache-manifest.d901674c63b40e69e6b1124147776bf1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c4e7ffb7586ba849acb7b24804daa01",
+    "url": "/facerecognitionbrain/index.html"
+  },
+  {
+    "revision": "212d41701fbf0190aad2",
+    "url": "/facerecognitionbrain/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "666f04442d9afee59dc2",
+    "url": "/facerecognitionbrain/static/css/main.c22aa6b2.chunk.css"
+  },
+  {
+    "revision": "212d41701fbf0190aad2",
+    "url": "/facerecognitionbrain/static/js/2.4d2bba70.chunk.js"
+  },
+  {
+    "revision": "666f04442d9afee59dc2",
+    "url": "/facerecognitionbrain/static/js/main.50acce92.chunk.js"
+  },
+  {
+    "revision": "ce2d7a8efc8c8d84e679",
+    "url": "/facerecognitionbrain/static/js/runtime~main.2d683c34.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/facerecognitionbrain/precache-manifest.ed41fa5077adbd7718fdff3677de4644.js"
+  "/facerecognitionbrain/precache-manifest.d901674c63b40e69e6b1124147776bf1.js"
 );
 
 self.addEventListener('message', (event) => {
