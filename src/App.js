a8c4beba86f9eff381306eb726057c579bf86b38
barrylee111 <barrylee115@gmail.com>
2019-04-28 20:25:34 -0600

    Added FaceRecognition.js and API for FR

--- src/App.js
+++ src/App.js
@@ -4,7 +4,13 @@ import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import Particles from 'react-particles-js';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import Clarifai from 'clarifai';
 import './App.css';
+
+const app = new Clarifai.App({
+ apiKey: '3f7cc67aafc848999a9ed60e79436f8a'
+});
 
 const particlesOptions={
   particles: {
@@ -18,6 +24,29 @@ const particlesOptions={
   }
 }
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+
+  onButtonSubmit = () =>{
+    console.log('click')
+    app.models.predict("a403429f2ddf4b49b307e318f00e528b", "https://samples.clarifai.com/face-det.jpg").then(
+    function(response) {
+      console.log(response);
+    },
+    function(err) {
+      // there was an error
+    }
+  );
+  }
+
   render(){
     return (
       <div className="App">
@@ -27,8 +56,11 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
-        {/*<FaceRecognition />*/}
+        <ImageLinkForm 
+          onInputChange={this.onInputChange} 
+          onButtonSubmit={this.onButtonSubmit}
+          />
+        <FaceRecognition />
       </div>
     );
   }
